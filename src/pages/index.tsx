b8d3f8b8fbfa1af3036c237f302f8d403d67e569
Leonardo Galindo <galindocode@gmail.com>
2023-02-06 10:11:21 -0400

    next 13, fa pro, tailwind 3.2.4

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFontAwesome } from "@fortawesome/pro-regular-svg-icons";
+
+const Home = () => {
+	return (
+		<div>
+			Lorem, ipsum dolor. <FontAwesomeIcon icon={faFontAwesome} />{" "}
+		</div>
+	);
+};
+
+export default Home;
